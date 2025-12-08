5e481591530fb4a36bea60b0a05c397697577709
Manav Patel <manavpatel2052@gmail.com>
2025-12-07 16:44:40 -0800

    snowflake_config.py

--- dags/config/snowflake_config.py
+++ dags/config/snowflake_config.py
@@ -7,7 +7,7 @@ from airflow.models import Variable
 # Snowflake connection parameters
 SNOWFLAKE_CONN_ID = 'snowflake_conn'
 
-# Snowflake database and schema (matching your Airflow connection)
+# Snowflake database and schema 
 SNOWFLAKE_DATABASE = Variable.get('SNOWFLAKE_DATABASE', default_var='USER_DB_HYENA')
 SNOWFLAKE_SCHEMA = Variable.get('SNOWFLAKE_SCHEMA', default_var='ANALYTICS')
 SNOWFLAKE_WAREHOUSE = Variable.get('SNOWFLAKE_WAREHOUSE', default_var='HYENA_QUERRY_WH')
